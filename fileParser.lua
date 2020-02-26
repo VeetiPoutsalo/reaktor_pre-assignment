bf3d9099fc4c2f43c7b7f9876e6f663e36da7f04
Veeti Poutsalo <veeti@poutsalo.com>
2020-02-26 22:43:20 +0200

    Implement parsing of dependencies

--- fileParser.lua
+++ fileParser.lua
@@ -5,6 +5,21 @@ return function(filename)
         if field then
             if field == "Package" then
                 packages[#packages + 1] = {[field] = data}
+            elseif field == "Depends" then
+                data = data:gsub("%s?%(.-%)%s?", "")
+                local dependencies = {}
+                for dependency in data:gmatch("([^,]*)") do
+                    if dependency ~= "" then
+                        local alternatives = {}
+                        for alternative in dependency:gmatch("([^|]*)") do
+                            if alternative ~= "" then
+                                alternatives[#alternatives + 1] = alternative
+                            end
+                        end
+                        dependencies[#dependencies + 1] = alternatives
+                    end
+                end
+                packages[#packages][field] = dependencies
             else
                 assert(packages[#packages])
                 packages[#packages][field] = data
