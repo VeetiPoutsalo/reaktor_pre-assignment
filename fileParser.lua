85fa8e5d266276ffcf2a88448ef9f43181346dc7
Veeti Poutsalo <veeti@poutsalo.com>
2020-02-26 23:10:17 +0200

    Implement reverse dependencies in fileParser
    While parsing packages a table is created with package names as
    indices, which is used in a second pass to add the reverse dependencies

--- fileParser.lua
+++ fileParser.lua
@@ -1,10 +1,13 @@
 return function(filename)
+    local function trim(str) return string.match(str, "^%s*(.-)%s*$") end
     local packages = {}
+    local packagesByName = {}
     for line in io.lines(filename) do
         local field, data = line:match("(.-): (.*)")
         if field then
             if field == "Package" then
                 packages[#packages + 1] = {[field] = data}
+                packagesByName[data] = packages[#packages]
             elseif field == "Depends" then
                 data = data:gsub("%s?%(.-%)%s?", "")
                 local dependencies = {}
@@ -13,7 +16,7 @@ return function(filename)
                         local alternatives = {}
                         for alternative in dependency:gmatch("([^|]*)") do
                             if alternative ~= "" then
-                                alternatives[#alternatives + 1] = alternative
+                                alternatives[#alternatives + 1] = trim(alternative)
                             end
                         end
                         dependencies[#dependencies + 1] = alternatives
@@ -23,6 +26,24 @@ return function(filename)
             else
                 assert(packages[#packages])
                 packages[#packages][field] = data
+            end
+        end
+    end
+    for i = 1, #packages do
+        local package = packages[i]
+        if package["Depends"] then
+            for j = 1, #package["Depends"] do
+                for k = 1, #package["Depends"][j] do
+                    local dependency = package["Depends"][j][k]
+                    if packagesByName[dependency] then
+                        local reverseDepends = packagesByName[dependency]["Reverse Depends"]
+                        if reverseDepends then
+                            reverseDepends[#reverseDepends + 1] = {package["Package"]}
+                        else
+                            packagesByName[dependency]["Reverse Depends"] = {{package["Package"]}}
+                        end
+                    end
+                end
             end
         end
     end
