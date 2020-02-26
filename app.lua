ea113abb6e170e72f886ce15c05e141c0f32898b
Veeti Poutsalo <veeti@poutsalo.com>
2020-02-27 01:05:49 +0200

    Implement comments

--- app.lua
+++ app.lua
@@ -1,28 +1,29 @@
 local lapis = require("lapis")
 local app = lapis.Application()
-app:enable("etlua")
-app.layout = require("views.layout")
+app:enable("etlua")                                 -- Enable etlua templates
+app.layout = require("views.layout")                -- Use etlua layout template
 
+--[ Package processing ]--
 local fileParser = require("fileParser")
 local packages, packagesByName = fileParser("/var/lib/dpkg/status")
-table.sort(packages, function(a, b)
+table.sort(packages, function(a, b)                 -- Sort packages alphabetically
   return a["Package"] < b["Package"]
 end)
 
 app:get("index", "/", function(self)
   self.packages = packages
-  return { render = "index" }
+  return { render = "index" }                       -- Render "index" template
 end)
 
 app:get("package", "/package/:name", function(self)
-  local package = packagesByName[self.params.name]
+  local package = packagesByName[self.params.name]  -- Look for the package in our list
   if not package then
-    self:write({"Not Found", status = 404})
+    self:write({"Not Found", status = 404})         -- Returns a basic "Not Found" message
   else
     self.package = package
     self.packagesByName = packagesByName
     self.page_title = string.format("Package: %s", package["Package"])
-    return { render = "package" }
+    return { render = "package" }                   -- Render "package" template
   end
 end)
 
--- fileParser.lua
+++ fileParser.lua
@@ -1,55 +1,54 @@
 return function(filename)
-    local function trim(str) return string.match(str, "^%s*(.-)%s*$") end
+    local function trim(str) return string.match(str, "^%s*(.-)%s*$") end   -- Trim extra whitespace from the beginning and end of the given string
     local packages = {}
-    local packagesByName = {}
+    local packagesByName = {}                                               -- Name-indexed table
     local lastField
-    for line in io.lines(filename) do
-        local field, data = line:match("(.-): (.*)")
+    for line in io.lines(filename) do                                       -- Loop over all lines in the file (This should work for huge files too)
+        local field, data = line:match("(.-): (.*)")                        -- Extract field and data using regex
         if field then
             lastField = field
-            if field == "Package" then
+            if field == "Package" then                                      -- "Package" field creates a new package entry
                 packages[#packages + 1] = {[field] = data}
-                packagesByName[data] = packages[#packages]
-            elseif field == "Depends" then
-                data = data:gsub("%s?%(.-%)%s?", "")
+                packagesByName[data] = packages[#packages]                  -- The new entry is also added to the name-indexed table
+            elseif field == "Depends" then                                  -- Parse dependencies
+                data = data:gsub("%s?%(.-%)%s?", "")                        -- Remove version numbers as we don't need them
                 local dependencies = {}
-                for dependency in data:gmatch("([^,]*)") do
+                for dependency in data:gmatch("([^,]*)") do                 -- Extract dependencies separated by a comma
                     if dependency ~= "" then
                         local alternatives = {}
-                        for alternative in dependency:gmatch("([^|]*)") do
+                        for alternative in dependency:gmatch("([^|]*)") do  -- Extract each alternative dependency separated by a "|" character
                             if alternative ~= "" then
                                 alternatives[#alternatives + 1] = trim(alternative)
                             end
                         end
-                        dependencies[#dependencies + 1] = alternatives
+                        dependencies[#dependencies + 1] = alternatives      -- The dependencies are added to the list as a table of alternatives
                     end
                 end
-                packages[#packages][field] = dependencies
+                packages[#packages][field] = dependencies                   -- Add the dependency list to the package
             else
-                assert(packages[#packages])
-                packages[#packages][field] = data
+                packages[#packages][field] = data                           -- If no special handling is defined, just stuff the whole data into the correct field
             end
-        elseif lastField == "Description" then
+        elseif lastField == "Description" then                              -- If a new field wasn't defined, parse long comment
             local content = trim(line)
             if content ~= "" then
-                packages[#packages]["Long Description"] = packages[#packages]["Long Description"] or {""}
+                packages[#packages]["Long Description"] = packages[#packages]["Long Description"] or {""}   -- Create new field if it doesn't exist
                 local longDesc = packages[#packages]["Long Description"]
-                if content == "." then
-                    longDesc[#longDesc + 1] = ""
+                if content == "." then                                                                      -- A "." line is a line break
+                    longDesc[#longDesc + 1] = ""                                                            -- An empty line is added twice as new content will be concatenated into the second one
                     longDesc[#longDesc + 1] = ""
                 else
-                    longDesc[#longDesc] = longDesc[#longDesc] .. " " .. content
+                    longDesc[#longDesc] = longDesc[#longDesc] .. " " .. content                             -- Concatenate the comment content to the previous line
                 end
             end
         end
     end
-    for i = 1, #packages do
+    for i = 1, #packages do                                                 -- Iterate all packages for reverse dependencies
         local package = packages[i]
         if package["Depends"] then
             for j = 1, #package["Depends"] do
-                for k = 1, #package["Depends"][j] do
+                for k = 1, #package["Depends"][j] do                        -- Iterate the package's all dependencies, including alternatives
                     local dependency = package["Depends"][j][k]
-                    if packagesByName[dependency] then
+                    if packagesByName[dependency] then                      -- If the dependency exists in our package list, we can add the reverse dependency
                         local reverseDepends = packagesByName[dependency]["Reverse Depends"]
                         if reverseDepends then
                             reverseDepends[#reverseDepends + 1] = {package["Package"]}
@@ -61,5 +60,5 @@ return function(filename)
             end
         end
     end
-    return packages, packagesByName
+    return packages, packagesByName                                         -- Return both the package list and the name-indexed list
 end
